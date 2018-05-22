e30ac423ecd1b52597e0d1b005c61d3b4c4ce3de
alif112 <alifalamin4@gmail.com>
2018-05-22 13:29:30 +0600

    tested socket started

---
+++ routes/chat.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+
+ var home = require('../app/controllers/home');
+
+
+var Chat= require('../app/controllers/chat_controller');
+
+
+
+
+router.get('/enter',home.loggedIn,Chat.enter);
+
+exports = module.exports =router;
+
